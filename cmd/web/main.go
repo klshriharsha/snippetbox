4ef35e0487ae6f604f3fa89c00793e87e5848954
Shriharsha KL <klshriharsha@gmail.com>
2024-03-16 20:00:43 +0530

    setup SQL connection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 
+	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/klshriharsha/snippetbox/cmd/web/config"
 )
 
@@ -13,11 +16,23 @@ func main() {
 	// setup a commandline flag to override the network address.
 	// run `go run ./cmd/web -help` for documentation on commandline flags
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	pgURL := flag.String(
+		"pgurl",
+		"postgresql://web:3c523592-852d-42be-915c-d5931792e39e@localhost:5432/postgres",
+		"PostgreSQL URL",
+	)
 	flag.Parse()
 
 	// create informational and error loggers
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// attempt to connect to the PostgreSQL database
+	db, err := connectDB(*pgURL)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
 
 	// for injecting dependencies to handlers
 	app := &config.Application{
@@ -33,6 +48,24 @@ func main() {
 	}
 
 	infoLog.Printf("starting server on %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+// connectDB creates an SQL connection pool and ensures a successful ping
+func connectDB(pgURL string) (*sql.DB, error) {
+	parsedURL, err := url.Parse(pgURL)
+	if err != nil {
+		return nil, err
+	}
+
+	conn, err := sql.Open("pgx", parsedURL.String())
+	if err != nil {
+		return nil, err
+	}
+	if err := conn.Ping(); err != nil {
+		return nil, err
+	}
+
+	return conn, nil
 }
